dff236a56fdf3d45cd8e6f23ec10cfe3ec9e68ea
techConsul97 <ittechconsultantemail@gmail.com>
2022-08-17 12:43:08 +0100

    Integrated mock and espresso testing

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/fake/FakeDataSource.kt
@@ -0,0 +1,71 @@
+package com.example.android.architecture.blueprints.todoapp.data.source.fake
+
+import androidx.lifecycle.LiveData
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
+import com.example.android.architecture.blueprints.todoapp.data.Result.Error
+
+
+
+class FakeDataSource(var tasks:MutableList<Task>? = mutableListOf()) : TasksDataSource {
+    override fun observeTasks(): LiveData<Result<List<Task>>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTasks(): Result<List<Task>> {
+        //get all tasks
+
+        tasks?.let { return Result.Success(ArrayList(it)) }
+        return Error(Exception("Tasks not found")
+        )
+    }
+
+    override suspend fun refreshTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeTask(taskId: String): LiveData<Result<Task>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTask(taskId: String): Result<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun refreshTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveTask(task: Task) {
+        tasks?.add(task)
+    }
+
+    override suspend fun completeTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun completeTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun clearCompletedTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllTasks() {
+        tasks?.clear()
+    }
+
+    override suspend fun deleteTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+}
